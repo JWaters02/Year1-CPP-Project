8675299ddd7d7f61fc2455105c2545afeecdb5c8
bobby45800 <44036776+bobby45800@users.noreply.github.com>
2021-03-03 20:29:30 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Year1_CPP_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Year1_CPP_Project.dir/main.cpp.obj"
+  "Year1_CPP_Project.exe"
+  "Year1_CPP_Project.exe.manifest"
+  "Year1_CPP_Project.lib"
+  "Year1_CPP_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Year1_CPP_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
