821167193aaaa5b893e29221da4cca3594dc684f
bobby45800 <44036776+bobby45800@users.noreply.github.com>
2021-03-04 16:33:38 +0000

    Rough outline of shopper class

--- Shopper.cpp
+++ Shopper.cpp
@@ -3,5 +3,77 @@
  * MANAGES PARSING OF COMMANDS
  * */
 
-
 #include "Shopper.h"
+
+//region Constructor
+Shopper::Shopper(double _totalSpent, std::vector<std::string> _itemsPurchased) {
+    history.totalSpent = _totalSpent;
+    history.itemsPurchased = _itemsPurchased;
+}
+//endregion
+
+//region Setters
+std::string Shopper::setName() {
+    int _numRandomNames = nameBank.size();
+    std::string selectedName = nameBank[rand() % _numRandomNames];
+    return selectedName;
+};
+
+int Shopper::setHeight() {
+    return rand() % (MAXHEIGHT - MINHEIGHT) + MINHEIGHT;
+}
+
+int Shopper::setWeight() {
+    return rand() % (MAXWEIGHT - MINWEIGHT) + MINWEIGHT;
+}
+
+int Shopper::setAge() {
+    return rand() % MAXAGE;
+}
+
+// Needs to make sure it does not share the same ID with other shoppers
+int Shopper::setID() {
+    bool isNewID = true;
+    int possibleID;
+    while (isNewID) {
+        possibleID = rand() % MAXID;
+        for (int i = 0; i < IDs.size(); i++) {
+            if (possibleID == IDs[i]) {
+                isNewID = false;
+            }
+        }
+    }
+    return possibleID;
+}
+
+bool Shopper::setIsInStore(bool _isInStore) {
+    isInStore = _isInStore;
+}
+//endregion
+
+//region Getters
+bool Shopper::getIsInStore() const {
+    return isInStore;
+}
+
+std::string Shopper::getName() {
+    return name;
+}
+
+int Shopper::getHeight() {
+    return height;
+}
+
+int Shopper::getWeight() {
+    return weight;
+}
+
+int Shopper::getAge() {
+    return age;
+}
+
+int Shopper::getID() {
+    return ID;
+}
+//endregion
+
--- Shopper.h
+++ Shopper.h
@@ -1,14 +1,64 @@
 /*
  * THIS CLASS MANAGES THE SHOPPERS
  * EACH SHOPPER HAS ITS OWN RANDOM SET OF VALUES WHEN INSTANTIATED
+ * SHOPPER IS NEVER CHANGED?
+ * THEREFORE IS NOT REQUIRED TO BE INSTANTIATED WITH DIFFERENT VALUES
  * */
 
 #ifndef YEAR1_CPP_PROJECT_SHOPPER_H
 #define YEAR1_CPP_PROJECT_SHOPPER_H
 
+#include <iostream>
+#include <vector>
+#include <random>
 
 class Shopper {
+public:
+    Shopper(double _totalSpent, std::vector<std::string> _itemsPurchased);
 
+    bool setIsInStore(bool _isInStore);
+    bool getIsInStore() const;
+
+    // Getters
+    std::string getName();
+    int getHeight();
+    int getWeight();
+    int getAge();
+    int getID();
+
+    // Purchase history is taken from a pre-existing shopper when created
+    struct purchaseHistory {
+        double totalSpent;
+        std::vector<std::string> itemsPurchased; // TODO: Use item objects from stock class
+    } history;
+private:
+    // Setters
+    std::string setName();
+    int setHeight();
+    int setWeight();
+    int setAge();
+    int setID();
+
+    // Consts
+    const int MAXHEIGHT = 240;
+    const int MINHEIGHT = 90;
+    const int MAXWEIGHT = 200;
+    const int MINWEIGHT = 15;
+    const int MAXAGE = 120;
+    const int MAXID = 10000;
+
+    std::string name;
+    std::vector<std::string> nameBank; // TODO: Get names from files class
+    std::vector<int> IDs; // TODO: Get IDs from files class
+    int height;
+    int weight;
+    int age;
+    int ID;
+    struct currentItemsInBasket {
+        double costOfItems;
+        std::vector<std::string> items; // TODO: Use item objects from stock class
+    };
+    bool isInStore;
 };
 
 
